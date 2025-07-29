cbd771f1c33fb3d6cf3caece3bb20ffe50247850
Mohammed Ahmed <46822007+MohamedAhmed35@users.noreply.github.com>
2025-07-29 17:04:41 +0300

    Update cls_train.py

--- text-classification/atis-intent-classifier/model/cls_train.py
+++ text-classification/atis-intent-classifier/model/cls_train.py
@@ -2,44 +2,41 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from feature_extraction import feature_gen
+
 from torch.utils.data import Dataset, DataLoader
 
 
-class TextDataset(DataLoader):
+class TextDataset(Dataset):
     """
     Custom Dataset for texts and labels
     """
-    def __init__(self, texts, labels):
-        self.texts = texts
-        self.labels = labels
+    def __init__(self, train = True):
+        """
+        Load train or test split.
+        If train=True  → loads atis_intents_train.csv
+        If train=False → loads atis_intents_test.csv
+        """
+        split = 'train' if train else 'test'
+        self.df = pd.read_csv(f'../data/atis_intents_{split}.csv')
+
+        self.texts = self.df['text'].values
+        self.labels = self.df['intent'].values
+
+        self.n_samples = self.df.shape[0]
 
     def __len__(self):
-        return len(self.texts)
+        return self.n_samples
 
     def __getitem__(self, idx):
         return self.texts[idx], self.labels[idx]
 
 
-# Load dataset
-df = pd.read_csv('../data/atis_intents.csv', header=None, names=['intent', 'query'])
-texts = df['query'].values
-labels = df['intent'].values
-
-
-# Train/Test split
-texts_train, texts_test, labels_train, labels_test = train_test_split(texts, labels, test_size=0.2, random_state=42)
-
-test_data_dict = {"query": texts_test,
-                  "label": labels_test}
-
-pd.DataFrame(test_data_dict).to_csv("test_intents.csv", index=False)
 
 # Prepare datasets & loaders
-train_dataset = TextDataset(texts_train, labels_train)
-test_dataset = TextDataset(texts_test, labels_test)
+train_dataset = TextDataset(train=True)
+test_dataset = TextDataset(train=False)
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size = 32, shuffle = False)
